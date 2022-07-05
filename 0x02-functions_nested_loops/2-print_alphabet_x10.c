ba52390facfd4d742d6a20e4b1fa5ab81e5ca5a6
jeffrey <jeffrey_uwenor@yahoo.com>
2022-07-05 16:10:38 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,13 @@ void print_alphabet_x10(void)
 {
 int  i;
 char c;
+
 for (i = 0; i < 10; i++)
-
-for (c = 'a'; c <= z; c++)
-
+{
+for (c = 'a'; c <= 'z'; c++)
+{
 _putchar(c);
+}
 _putchar('\n');
 }
-
+}
