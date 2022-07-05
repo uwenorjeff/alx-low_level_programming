5083a67de2543e667b53e3af060df54147bf9356
jeffrey <jeffrey_uwenor@yahoo.com>
2022-07-05 07:53:01 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include main.h
+/**
+ * main
+ * A program that prints _putchar
+ * Return: 0 for success
+ */
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return(0);
+}
