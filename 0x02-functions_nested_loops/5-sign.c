acc146896e27b40a17e2cce6bd198c24f2ea429e
jeffrey <jeffrey_uwenor@yahoo.com>
2022-07-06 16:42:00 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - a function that prints the sign of a number
- * n: number input
+ * @n: number input
  * Return: 1 prints '+' if n > 0, 0 prints '0' if n = 0, -1 prints '-' if n < 0
  */
 
