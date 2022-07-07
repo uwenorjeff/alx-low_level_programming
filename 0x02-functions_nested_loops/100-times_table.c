08c238fb9bdb051e177a52e4ff78708b5ca57ebb
jeffrey <jeffrey_uwenor@yahoo.com>
2022-07-06 18:43:46 -0700

    Multiplication table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+
+
+
+/* *
+ * print_times_table - prints the times table for n.
+ *  @n: The multiplication table.
+ *  Return: Nothing.
+ */
+
+void print_times_table(int n)
+
+{
+int i, y, res;
+if (!(n > 15 || n < 0))
+{
+for (i = 0; i <= n; i++)
+{
+for (y = 0; y <= n; y++)
+{
+res = (i * y);
+if (y != 0)
+{
+_putchar(',');
+_putchar(' ');
+}
+if (res < 10 && y != 0)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar((res % 10) + '0');
+}
+else if (res >= 10 && res < 100)
+{
+_putchar(' ');
+_putchar((res / 10) + '0');
+_putchar((res % 10) + '0');
+}
+else if (res >= 100 && y != 0)
+{
+_putchar((res / 100) + '0');
+_putchar((res / 10) % 10 + '0');
+_putchar((res % 10) + '0');
+}
+else
+_putchar((res % 10) + '0');
+}
+_putchar('\n');
+}
+}
+}
