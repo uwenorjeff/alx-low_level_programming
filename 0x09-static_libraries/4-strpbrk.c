1536b5eff7ca9a8ddc6a19209b3a21d5e43fc2b9
jeffrey <jeffrey_uwenor@yahoo.com>
2022-07-22 16:28:57 -0700

    libmy

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,1 +1,27 @@
-char *_strpbrk(char *s, char *accept);
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strpbrk - prints the consecutive caracters of s1 that are in s2.
+ * @s: source string
+ * @accept: searching string
+ * Return: new string.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+unsigned int i, j;
+for (i = 0; *(s + i); i++)
+{
+for (j = 0; *(accept + j); j++)
+{
+if (*(s + i) == *(accept + j))
+{
+break;
+}
+}
+if (*(accept + j) != '\0')
+{
+return (s + i);
+}
+}
+return (0);
+}
