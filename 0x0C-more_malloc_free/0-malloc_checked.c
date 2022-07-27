ba7714d0aaff1708b42bde90d32fad434832cf15
jeffrey <jeffrey_uwenor@yahoo.com>
2022-07-27 03:23:32 -0700

    0-malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - creates an array of integers.
+ * @b: input.
+ * Return: Output.
+ */
+void *malloc_checked(unsigned int b)
+{
+void *y = malloc(b);
+if (y == NULL)
+{
+exit(98);
+}
+return (y);
+}
