e9a6de8356c163fcab1e8de69b19bbb2665794e8
jeffrey <jeffrey_uwenor@yahoo.com>
2022-07-28 08:14:09 -0700

    101-mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -86,17 +86,17 @@ int get_digit(char c)
 /**
  * get_prod - Multiplies a string of numbers by a single digit.
  * @prod: The buffer to store the result.
- * @mult: The string of numbers.
+ * @mul: The string of numbers.
  * @digit: The single digit.
  * @zeroes: The necessary number of leading zeroes.
  * Description: If mul contains a non-digit, the function
  * exits with a status value of 98.
  */
-void get_prod(char *prod, char *mult, int digit, int zeroes)
+void get_prod(char *prod, char *mul, int digit, int zeroes)
 {
-int mult_len, num, tens = 0;
-mult_len = find_len(mult) - 1;
-mult += mult_len;
+int mul_len, num, tens = 0;
+mul_len = find_len(mul) - 1;
+mul += mul_len;
 while (*prod)
 {
 *prod = 'x';
@@ -108,14 +108,14 @@ while (zeroes--)
 *prod = '0';
 prod--;
 }
-for (; mult_len >= 0; mult_len--, mult--, prod--)
+for (; mul_len >= 0; mul_len--, mul--, prod--)
 {
-if (*mult < '0' || *mult > '9')
+if (*mul < '0' || *mul > '9')
 {
 printf("Error\n");
 exit(98);
 }
-num = (*mult - '0') * digit;
+num = (*mul - '0') * digit;
 num += tens;
 *prod = (num % 10) + '0';
 tens = num / 10;
