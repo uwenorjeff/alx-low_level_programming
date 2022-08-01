78f5fa0a95eb634c8eead1889bb8e37dc1e04850
jeffrey <jeffrey_uwenor@yahoo.com>
2022-08-01 15:42:45 -0700

    4-new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,31 +13,18 @@
 char *dup_string(char *s)
 
 {
-
-	int len, i;
-
-	char *ret;
-
-	for (len = 0; s[len]; len++)
-
-		;
-
-	ret = malloc(sizeof(char) * (len + 1));
-
-	if (ret == NULL)
-
-		return (NULL);
-
-	for (i = 0; s[i]; i++)
-
-		ret[i] = s[i];
-
-	ret[i] = '\0';
-
-	return (ret);
-
+int len, i;
+char *ret;
+for (len = 0; s[len]; len++)
+;
+ret = malloc(sizeof(char) * (len + 1));
+if (ret == NULL)
+return (NULL);
+for (i = 0; s[i]; i++)
+ret[i] = s[i];
+ret[i] = '\0';
+return (ret);
 }
-
 
 
 /**
@@ -53,55 +40,30 @@ char *dup_string(char *s)
 dog_t *new_dog(char *name, float age, char *owner)
 
 {
-
-	char *tname, *towner;
-
-	dog_t *ret;
-
-	if (name == NULL || owner == NULL)
-
-		return (NULL);
-
-	ret = malloc(sizeof(dog_t));
-
-	if (ret == NULL)
-
-		return (NULL);
-
-	ret->age = age;
-
-	tname = dup_string(name);
-
-	if (tname == NULL)
-
-	{
-
-		free_dog(ret);
-
-		return (NULL);
-
-	}
-
-	ret->name = tname;
-
-	towner = dup_string(owner);
-
-	if (towner == NULL)
-
-	{
-
-		free_dog(ret);
-
-		return (NULL);
-
-	}
-
-	ret->owner = towner;
-
-	return (ret);
-
+char *tname, *towner;
+dog_t *ret;
+if (name == NULL || owner == NULL)
+return (NULL);
+ret = malloc(sizeof(dog_t));
+if (ret == NULL)
+return (NULL);
+ret->age = age;
+tname = dup_string(name);
+if (tname == NULL)
+{
+free_dog(ret);
+return (NULL);
 }
-
+ret->name = tname;
+towner = dup_string(owner);
+if (towner == NULL)
+{
+free_dog(ret);
+return (NULL);
+}
+ret->owner = towner;
+return (ret);
+}
 
 
 /**
@@ -112,19 +74,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 void free_dog(dog_t *d)
 
 {
-
-	if (d == NULL)
-
-		return;
-
-	if (d->name != NULL)
-
-		free(d->name);
-
-	if (d->owner != NULL)
-
-		free(d->owner);
-
-	free(d);
-
+if (d == NULL)
+return;
+if (d->name != NULL)
+free(d->name);
+if (d->owner != NULL)
+free(d->owner);
+free(d);
 }
