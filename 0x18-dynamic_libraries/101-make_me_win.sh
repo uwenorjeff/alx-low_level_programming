f3dc13ca4807101b5b0f26b891c4b86f8d20781d
uwenorjeff <jeffrey_uwenor@yahoo.com>
2022-10-09 09:32:09 -0700

    corrected exercise 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/uwenorjeff/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/putshack.so
+wget -O /tmp/putshack.so https://github.com/uwenorjeff/alx-low_level_programming/blob/main/0x18-dynamic_libraries/putshack.so
 export LD_PRELOAD=/tmp/putshack.so
