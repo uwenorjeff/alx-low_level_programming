cfa29a7140aaea291240fcde3be82fad0e1560d2
uwenorjeff <106762251+uwenorjeff@users.noreply.github.com>
2022-10-26 11:49:48 +0100

    0-hash_table_create

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,29 +1,18 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_create - creates a hash table
- * @size: size of the array
- * Return: a pointer to the newly created hash table
+ * hash_table_create - create a hash table
+ * @size: Table size
+ * Return: success
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht = NULL;
-	unsigned long int i = 0;
-
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
-		return (NULL);
-
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
-		return (NULL);
-
-	ht->size = size;
-	while (i < size)
-	{
-		ht->array[i] = NULL;
-		i++;
-	}
-
-	return (ht);
+hash_table_t *table;
+table = malloc(sizeof(hash_table_t));
+if (table == NULL)
+return (NULL);
+table->size = size;
+table->array = malloc(sizeof(table->array) * size);
+if (table->array == NULL)
+return (NULL);
+return (table);
 }
